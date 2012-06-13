2f7409c39b90ddc48aab82f76ff2154026487d94
Dominic <dominic.wanner@fh-heidelberg.de>
2012-06-13 07:07:04 +0200

    Anfang von Kommunikater

--- src/Kommunikater.h
+++ src/Kommunikater.h
@@ -5,5 +5,35 @@ Kommunikater.h
 Autor: Dominic Wanner
 
 Beschreibung:
+------------------
+Dieser teil wählt sich im Server ein und gibt nachrichten nach ausen und hört zu
 */
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "../bib/libircclient-1.6/include/libircclient.h"
+#include "Datenbank.h"
+
+struct Im
+{
+    char 	* channel;
+	char 	* nick;
+	char    * server;
+};
+
+class Kommunikater {
+    public:
+    Kommunikater();
+    ~Kommunikater();
+
+    void commitServer();
+    void commitChanel();
+
+    void chanceChanel();
+    void chanceServer();
+
+    void sendMassage();
+    void listen();
+
+    private:
+    SQL* Kom;
+    SQL* Doc;
+};
